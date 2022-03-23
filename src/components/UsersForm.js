62a2e81a5f708097aeb2679e806491339e097642
Jhonatan-Ordonez <jhoeduardonez@gmail.com>
2022-03-22 22:38:50 -0500

    fixed dependencies

--- src/components/UsersForm.js
+++ src/components/UsersForm.js
@@ -37,7 +37,7 @@ const UsersForm = ({ getUsers, userSelect, setUserSelect, selectU }) => {
             setStates()
         }
         
-    }, [userSelect])
+    }, [userSelect, setStates() ])
 
     // console.log(userSelect);
 
